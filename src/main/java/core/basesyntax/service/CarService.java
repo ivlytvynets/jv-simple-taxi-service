3ebf1466a547c30dd5c38986a11c7f7e564ea2bf
ivan-lytvynets <ivlitvinets@gmail.com>
2021-01-18 12:53:49 +0200

    implemented GenericService, edited codestyle

--- src/main/java/core/basesyntax/service/CarService.java
+++ src/main/java/core/basesyntax/service/CarService.java
@@ -4,18 +4,8 @@ import core.basesyntax.model.Car;
 import core.basesyntax.model.Driver;
 import java.util.List;
 
-public interface CarService {
-    Car create(Car car);
-
-    Car get(Long id);
-
-    List<Car> getAll();
-
+public interface CarService extends GenericService<Car, Long> {
     List<Car> getAllByDriver(Long driverId);
-
-    Car update(Car car);
-
-    boolean delete(Long id);
 
     void addDriverToCar(Driver driver, Car car);
 
